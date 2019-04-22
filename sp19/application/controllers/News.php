60de53558ada25da22a08c1748039537d36931f8
Craig Peterson <craig.b.peterson@gmail.com>
2019-04-21 17:23:29 -0700

    fixed the News controller
    Commented out the header and footer lines for the view() and create() methods in the News controller. The header and footer is now handled by the bootswatch theme in the views part of the app.

--- sp19/application/controllers/News.php
+++ sp19/application/controllers/News.php
@@ -30,9 +30,9 @@ class News extends CI_Controller {
 
         $data['title'] = $data['news_item']['title'];
 
-        $this->load->view('templates/header', $data);
+        //$this->load->view('templates/header', $data);
         $this->load->view('news/view', $data);
-        $this->load->view('templates/footer', $data);
+        //$this->load->view('templates/footer', $data);
     }
 
     public function create()
@@ -48,17 +48,17 @@ class News extends CI_Controller {
 
         if ($this->form_validation->run() === FALSE)
         {
-            $this->load->view('templates/header', $data);
+            //$this->load->view('templates/header', $data);
             $this->load->view('news/create', $data);
-            $this->load->view('templates/footer', $data);
+            //$this->load->view('templates/footer', $data);
 
         }
         else
         {
             $this->news_model->set_news();
-            $this->load->view('templates/header', $data);
+            //$this->load->view('templates/header', $data);
             $this->load->view('news/success', $data);
-            $this->load->view('templates/footer', $data);
+            //$this->load->view('templates/footer', $data);
         }
     }
 
